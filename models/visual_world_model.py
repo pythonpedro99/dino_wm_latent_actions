5e40e6a7007a1835d9d9edeb3b23dbe0977591ba
julianquast <julian.quast@fluxraum.de>
2026-01-19 16:54:00 +0100

    dev step

--- models/visual_world_model.py
+++ models/visual_world_model.py
@@ -374,6 +374,7 @@ class VWorldModel(nn.Module):
         return z
 
     def rollout(self, obs, act, num_obs_init):
+        
         if act is not None:
             act = torch.cat([act, act[:, -1:, ...]], dim=1)  # repeat last action
     
